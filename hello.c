96c1a891790c53c7191a9552bbc1cbfb993cb852
dejwis <dawid.smarduch@gmail.com>
2024-02-18 19:57:57 +0100

    tricky mouse

--- hello.c
+++ hello.c
@@ -2,6 +2,18 @@
 
 int main()
 {
-    printf("Hello, World!\n");
+    float a = 0.1;
+    float b = 0.2;
+    float suma = a + b;
+
+    if (suma = 0.3)
+    {
+        printf("Suma jest rowna 0.3\n");
+    }
+    else
+    {
+        printf("Suma nie jest rowna 0.3, suma to: %.1f\n", suma);
+    }
+
     return 0;
 }
